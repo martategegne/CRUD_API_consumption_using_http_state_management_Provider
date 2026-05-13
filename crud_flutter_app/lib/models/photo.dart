462520e7a42c4397cc22fa93ef3a4136ad086762
martategegne <mirandatefl2023@gmail.com>
2026-05-13 09:18:06 +0300

    Create Photo model with fromJson and toJson methods

---
+++ crud_flutter_app/lib/models/photo.dart
@@ -0,0 +1,27 @@
+class Photo {
+  final int id;
+  final String title;
+  final String url;
+
+  Photo({
+    required this.id,
+    required this.title,
+    required this.url,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'url': url,
+    };
+  }
+}
